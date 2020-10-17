1f8a3c11f076b854796528c4913dd00949f20789
Szymon <szymonjuraszek777@gmail.com>
2020-10-17 13:22:40 +0200

    Correct URL

--- globalConfig.ts
+++ globalConfig.ts
@@ -1,3 +1,3 @@
-export const URL_WEBSOCKET = 'ws://83.229.84.77:8080/rsocket';
-// export const URL_WEBSOCKET = 'ws://localhost:8080/rsocket';
+export const URL_WEBSOCKET = 'ws://83.229.84.77:8080/socket';
+// export const URL_WEBSOCKET = 'ws://localhost:8080/socket';
 export const STOP_SENDING_TIMEOUT = 110000;
